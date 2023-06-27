b0976ec6e1e074085027c0d864869135154af69a
minaxi <minaxijoshi3101@gmail.com>
2023-06-27 19:36:25 +0800

    Raffle game created

--- src/main/java/Raffle.java
+++ src/main/java/Raffle.java
@@ -1,36 +1,29 @@
+import javax.sql.rowset.spi.SyncResolver;
 import java.util.*;
 public class Raffle {
+    int groupNumber=0;
     double potSize = 0;
+    static int choice =0;
     String name;
     int numberOfTickets = 0;
     ArrayList<Integer> purchasedTickets;
     static ArrayList<Integer> winningTickets;
     private static Map<String, Integer> ticketCounts = new HashMap<>();
-    Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
     public void mainMenu(String status) {
         System.out.println(status);
         System.out.println("[1] Start a New Draw");
         System.out.println("[2] Buy Tickets");
         System.out.println("[3] Run Raffle");
-    /* if(status!= null || status!= "") {
-         status="";
-     } else {
-         //scanner = new Scanner(System.in);
-         int choice = scanner.nextInt();
-         game(choice);
-     }*/
-
     }
 
     private Set<Integer> generateWinningTicket() {
         Set<Integer> winningTicket = new HashSet<>();
         Random random = new Random();
-
         while (winningTicket.size() < 5) {
             int number = random.nextInt(15) + 1;
             winningTicket.add(number);
         }
-
         return winningTicket;
     }
     private Map<String, Integer> identifyWinners(Set<Integer> winningTicket) {
@@ -41,7 +34,7 @@ public class Raffle {
             int numTickets = entry.getValue();
             int numMatchingNumbers = getNumMatchingNumbers(winningTicket, numTickets);
 
-            if (numMatchingNumbers >= 2) {
+            if (numMatchingNumbers >= 1) {
                 winners.put(attendee, numMatchingNumbers);
             }
         }
@@ -61,7 +54,6 @@ public class Raffle {
                 }
             }
         }
-
         return numMatchingNumbers;
     }
     private Set<Integer> generateTicketNumbers() {
@@ -76,30 +68,30 @@ public class Raffle {
         return ticketNumbers;
     }
     private void displayWinners(Map<String, Integer> winners) {
-        System.out.println("Winners:");
+        System.out.println("Group "+groupNumber+" winners:");
 
         for (Map.Entry<String, Integer> entry : winners.entrySet()) {
             String attendee = entry.getKey();
             int numMatchingNumbers = entry.getValue();
             double reward = calculateReward(numMatchingNumbers);
-            System.out.println(attendee + ": " + numMatchingNumbers + " matching numbers. Reward: $" + reward);
+            System.out.println(attendee + " with "+numMatchingNumbers+" winning ticket(s)- $" + reward);
         }
     }
     private double calculateReward(int numMatchingNumbers) {
         double rewardPercentage;
 
         switch (numMatchingNumbers) {
+            case 1:
+                rewardPercentage = 10.0;
+                break;
             case 2:
-                rewardPercentage = 10.0; // Example reward percentage for Group 2
+                rewardPercentage = 25.0;
                 break;
             case 3:
-                rewardPercentage = 25.0; // Example reward percentage for Group 3
+                rewardPercentage = 50.0;
                 break;
             case 4:
-                rewardPercentage = 50.0; // Example reward percentage for Group 4
-                break;
-            case 5:
-                rewardPercentage = 100.0; // Example reward percentage for Group 5
+                rewardPercentage = 100.0;
                 break;
             default:
                 rewardPercentage = 0.0;
@@ -107,17 +99,19 @@ public class Raffle {
 
         return potSize * (rewardPercentage / 100.0);
     }
-    public void choice1_task()
+    public void startNewDraw()
     {
+        ticketCounts.clear();
         potSize = potSize + 100;
         System.out.println("New Raffle draw has been started. Initial pot size: $" + potSize +
                 "\nPress any key to return to main menu");
-        scanner.nextLine();
+        //new Scanner(System.in);
+        scanner.nextLine(); //Clear the newline character from the previous input
+        scanner.nextLine(); // Wait for any key press
         mainMenu("Welcome to My Raffle App \nStatus: Draw is ongoing. Raffle pot size is $" + potSize);
     }
-    public void choice2_task()
+    public void buyTickets()
     {
-        potSize = 0;
         System.out.print("Enter your name,");
         Scanner name_ticket = new Scanner(System.in);
         System.out.print(" no of tickets to purchase");
@@ -125,9 +119,8 @@ public class Raffle {
         numberOfTickets = name_ticket.nextInt();
          /*Each ticket should consist of 5 unique randomly generated numbers between [1-15] as mentioned in the
          Ticket Structure.*/
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        map.put(name, numberOfTickets);
-        System.out.println("Hi " + name + ", you have purchased" + numberOfTickets + " ticket(s)-");
+        ticketCounts.put(name, ticketCounts.getOrDefault(name, 0) + numberOfTickets);
+        System.out.println("Hi " + name + ", you have purchased " + numberOfTickets + " ticket(s)-");
         for (int t = 1; t <= numberOfTickets; t++) {
             purchasedTickets = new ArrayList<Integer>();
             for (int i = 1; i <= 15; i++) {
@@ -142,12 +135,13 @@ public class Raffle {
             System.out.print("\n");
             potSize = potSize + (t * 5);
         }
-        System.out.print("\n");
         System.out.println("Press any key to return to main menu");
-        pressAnyKeyLogic();
+        scanner.nextLine(); //Clear the newline character from the previous input
+        scanner.nextLine(); // Wait for any key press
         mainMenu("Welcome to My Raffle App \nStatus: Draw is ongoing. Raffle pot size is $" + potSize);
     }
-    private void choice3_runRaffle() {
+    private void runRaffle() {
+        groupNumber++;
         System.out.println("Running raffle...");
         // Generate the winning ticket
         Set<Integer> winningTicket = generateWinningTicket();
@@ -160,52 +154,45 @@ public class Raffle {
         Map<String, Integer> winners = identifyWinners(winningTicket);
         // Calculate rewards and display the winners
         displayWinners(winners);
-        potSize = 0.0;
+        //potSize = 0.0;
         ticketCounts.clear();
+        System.out.println("Press any key to return to main menu");
+        scanner.nextLine(); //Clear the newline character from the previous input
+        scanner.nextLine(); // Wait for any key press
+        mainMenu("Welcome to My Raffle App \nStatus: Status: Draw has not started. Initial pot size is $" + potSize);
     }
-    public void pressAnyKeyLogic()
+    public void enterChoice()
     {
-        scanner.nextLine();
+        choice = scanner.nextInt();
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         boolean exit = false;
         //console app that displays the following options (aka main menu) to users-
         //System.out.println("Welcome to My Raffle App \n");
         Raffle raf = new Raffle();
         raf.mainMenu("Welcome to My Raffle App \nStatus: Draw has not started \n");
-        int choice = raf.scanner.nextInt();
+        choice = scanner.nextInt();
         while (!exit) {
-            //if (currentChoice == 1) {
             switch (choice) {
                 case 1:
-                    if (choice ==1 ) {
-                        raf.choice1_task();
-                        raf.pressAnyKeyLogic();
-                    }
+                    raf.startNewDraw();
+                    raf.enterChoice();
                     break;
                 case 2:
-                    if (choice == 2) {
-                        raf.choice2_task();
-                        raf.pressAnyKeyLogic();
-                    }
+                    raf.buyTickets();
+                    raf.enterChoice();
                     break;
                 case 3:
-                    if (choice == 3) {
-                        raf.choice3_runRaffle();
-                    }
+                    raf.runRaffle();
+                    raf.enterChoice();
                     break;
                 default:
+                    System.out.println("Invalid choice. Please try again.");
+                    exit = true;
                     break;
             }
-            new Scanner(System.in);
-            // raf.game(choice);
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-            main(args);
         }
     }
 
 }
-
